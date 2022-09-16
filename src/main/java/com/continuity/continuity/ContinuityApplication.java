deb2de453a271c8a2e99e4f77895a9dc4ee22d04
Jesus <jesus.ambrocio@outlook.com>
2022-09-16 11:24:40 -0500

    Base push

---
+++ src/main/java/com/continuity/continuity/ContinuityApplication.java
@@ -0,0 +1,13 @@
+package com.continuity.continuity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/continuity/continuity/ContinuityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.continuity.continuity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContinuityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
