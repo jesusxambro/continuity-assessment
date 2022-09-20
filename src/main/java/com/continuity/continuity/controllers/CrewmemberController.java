d8a6c5e8c2298d97ad7de0218e7dbba08249918e
Jesus Ambro <68930496+jesusxambro@users.noreply.github.com>
2022-09-20 16:04:58 -0500

    Added four new methods

--- src/main/java/com/continuity/continuity/controllers/CrewmemberController.java
+++ src/main/java/com/continuity/continuity/controllers/CrewmemberController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
 
@@ -47,6 +48,35 @@ public class CrewmemberController {
         this.repository.deleteById(id);
         return new ResponseEntity<>(
                 String.format("The crewmember with an id of $d has been deleted from the database",id),HttpStatus.NO_CONTENT);
+    }
+    @PutMapping("/{id}")
+    public Crewmember putById(@RequestBody Crewmember updatedInfo, @PathVariable long id){
+        Crewmember crewToUpdate = this.repository.getReferenceById(id);
+        crewToUpdate.setName(updatedInfo.getName());
+        crewToUpdate.setMorale(updatedInfo.getMorale());
+        return this.repository.save(crewToUpdate);
+    }
+    @PatchMapping("/{id}")
+    public Crewmember patchById(@RequestBody Map<String, String> updatedInfo, @PathVariable long id){
+        Crewmember memberToUpdate = this.repository.getReferenceById(id);
+
+        updatedInfo.forEach((key, value) -> {
+            if(key.equals("name")){
+                updateName(memberToUpdate,value);
+            }
+            if(key.equals("morale")){
+                updateMorale(memberToUpdate,Integer.valueOf(value) );
+            }
+        });
+
+
+        return this.repository.save(memberToUpdate);
+    }
+    private void updateName(Crewmember member, String newName ){
+        member.setName(newName);
+    }
+    private void updateMorale(Crewmember member, Integer morale){
+        member.setMorale(morale);
     }
 
 
--- src/main/java/com/continuity/continuity/controllers/SpaceshipController.java
+++ src/main/java/com/continuity/continuity/controllers/SpaceshipController.java
@@ -2,12 +2,14 @@ package com.continuity.continuity.controllers;
 
 
 import com.continuity.continuity.dao.SpaceshipRepository;
+import com.continuity.continuity.model.Crewmember;
 import com.continuity.continuity.model.Spaceship;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 import java.util.NoSuchElementException;
 
 @RestController
@@ -47,6 +49,35 @@ public class SpaceshipController {
             return String.format("Your current spaceship has the id of %d", current.getId());
         }
 
+    }
+    @PutMapping("/{id}")
+    public Spaceship putById(@RequestBody Spaceship updatedInfo, @PathVariable long id){
+        Spaceship shipToUpdate = this.repository.getReferenceById(id);
+        shipToUpdate.setName(updatedInfo.getName());
+        shipToUpdate.setFuel(updatedInfo.getFuel());
+        return this.repository.save(shipToUpdate);
+    }
+    @PatchMapping("/{id}")
+    public Spaceship patchById(@RequestBody Map<String, String> updatedInfo, @PathVariable long id){
+        Spaceship shipToUpdate = this.repository.getReferenceById(id);
+
+        updatedInfo.forEach((key, value) -> {
+            if(key.equals("name")){
+                updateName(shipToUpdate,value);
+            }
+            if(key.equals("morale")){
+                updateFuel(shipToUpdate,Integer.valueOf(value) );
+            }
+        });
+
+
+        return this.repository.save(shipToUpdate);
+    }
+    private void updateName(Spaceship ship, String newName ){
+        ship.setName(newName);
+    }
+    private void updateFuel(Spaceship ship, Integer fuel){
+        ship.setFuel(fuel);
     }
 
 
